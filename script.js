c3d4b8d668454b384cdfed4450fc0d810dcc03c8
Tanner Nielsen <tan-ali5@outlook.com>
2020-05-02 08:53:40 -0700

    pseudo code for js

---
+++ script.js
@@ -0,0 +1,13 @@
+// load document
+
+
+// display current time and date from moments
+
+
+// save user input for certain time
+
+// save items to local storage
+
+// load data from local storage?
+
+// check over time blocks
