32a2559f6fe59790c21c4bcce3231980d75773cd
Tanner Nielsen <tan-ali5@outlook.com>
2020-05-02 18:05:37 -0700

    added document ready and set up current day time

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 // load document
+$(document).ready(function() {
+    event.preventDefault();
+    // display current time and date from moments
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    // save user input for certain time
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+
+    
+
+})
 
 
-// display current time and date from moments
 
 
-// save user input for certain time
+
 
 // save items to local storage
 
