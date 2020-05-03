4e1d50d8e661d968f31c27414c2a8c79b8a496a5
Tanner Nielsen <tan-ali5@outlook.com>
2020-05-02 18:19:36 -0700

    added all hours and track hour function

--- script.js
+++ script.js
@@ -12,7 +12,20 @@ $(document).ready(function() {
     })
 // load data from local storage?
 $("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour1 .description").val(localStorage.getItem("hour1"));
+$("#hour2 .description").val(localStorage.getItem("hour2"));
+$("#hour3 .description").val(localStorage.getItem("hour3"));
+$("#hour4 .description").val(localStorage.getItem("hour4"));
+$("#hour5 .description").val(localStorage.getItem("hour5"));
 
+// track hour/know what time it is
+
+function trackHour () {
+    
+}
 })
 
 
